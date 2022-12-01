a7c7fb96f57f3d66e11c73a1497907c716f70b9a
psycodebr <viniciussboscolo@gmail.com>
2022-11-30 23:31:47 -0300

    Apache v.1.0

---
+++ iactest2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+apt-get install wget -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
